65721f9cf86c77f68773767e84f2ba96f44cdae8
Jayant Kumar <jayantkumar13ssm@gmail.com>
2025-09-17 23:03:21 +0530

    Added Repositories

---
+++ src/main/java/com/example/demo/repositories/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repositories;
+
+import com.example.demo.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    List<Employee> findByEmail(String email);
+}
+
