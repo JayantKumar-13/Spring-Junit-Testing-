4a21392f08838acf58bf968631003d2bf5f717ca
Jayant Kumar <jayantkumar13ssm@gmail.com>
2025-09-17 22:45:14 +0530

    Added Advices

--- src/main/java/com/example/demo/advices/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/advices/GlobalExceptionHandler.java
@@ -1,2 +1,20 @@
-package com.example.demo.advices;public class GlobalExceptionHandler {
+package com.example.demo.advices;
+
+import com.example.demo.exceptions.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.internalServerError().build();
+    }
 }
